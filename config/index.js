b121664ed35ae7034a7b25e26224a5d44dc89834
KUN LI <li.kun1@northeastern.edu>
2023-12-06 21:44:52 -0500

    Update: connection

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     secretKey: 'b8dc437e2270ef8ceaf100abea1b0fd3cdec21c1d90a1be76ab678a47b4c764c',
-    dbURL: 'mongodb://localhost/INFO6150'
+    // dbURL: 'mongodb://localhost/INFO6150'
+    dbURL: 'mongodb+srv://likun9452:w875100@cluster0.tj3rqyx.mongodb.net/'
 }
