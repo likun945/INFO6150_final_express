34060dcb7f5f464a8c158c4bdc648784efcf106b
KUN LI <li.kun1@northeastern.edu>
2023-12-06 21:49:42 -0500

    test2

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', (req, res) => {
-    console.log(req)
+    return 'love you world'
 })
 app.use('/api', routes);
 
